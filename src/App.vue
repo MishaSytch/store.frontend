54ceacebf92e6c2d6e88c8237627cc3752dcfd25
Misha S. <misha.sytch@mail.ru>
2024-09-02 16:38:09 +0300

    Базовая стилизация

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
+<script setup>
+
+</script>
+
 <template>
-  <h1>Hi!</h1>
-  <div>
-    <h1>
-      Fff
-    </h1>
+  <div class="bg-amber-50 w-11/12 m-auto h-screen rounded-xl shadow-xl mt-5">
+    <header class="flex justify-between border-b border-slate-300 px-8">
+      <div class="flex items-center gap-4">
+        <img src="" alt="logo" class="w-4" />
+        <h2 class="">Каталог</h2>
+      </div>
+
+      <div>
+        <ul class="flex items-center gap-10">
+          <li class="flex items-center gap-2">
+            <h3>войти</h3>
+            <img src="" alt="person">
+          </li>
+          <li class="flex items-center gap-2">
+            <h3>корзина</h3>
+            <img src="" alt="cart">
+            <h3>(0)</h3>
+          </li>
+        </ul>
+      </div>
+    </header>
   </div>
 </template>
